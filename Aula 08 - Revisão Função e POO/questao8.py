97f1fa4d0147f2d9e3b3b43e3d7d969868a5cff7
liasarahgrude-rgb <liasarahgrude@gmail.com>
2026-05-30 08:48:02 -0300

    Add exercicios

--- Aula 08 - Revisão Função e POO/questao8.py
+++ Aula 08 - Revisão Função e POO/questao8.py
@@ -1,1 +1,23 @@
 ##Contexto: O setor de Recursos Humanos de uma startup precisa calcular a folha de pagamento de diferentes tipos de prestadores de serviço.Instruções:Herança: Crie uma classe base chamada Prestador. O construtor deve receber o nome do profissional. Essa classe deve ter um método chamado calcular_ganhos que retorna 0.0.Polimorfismo:Crie uma subclasse chamada Diarista que herda de Prestador. Seu construtor deve receber o nome, a quantidade_dias trabalhados e o valor_dia. Reescreva (override) o método calcular_ganhos para retornar a multiplicação dos dias pelo valor do dia.Crie outra subclasse chamada Mensalista que herda de Prestador. Seu construtor deve receber o nome e o salario_fixo. Reescreva o método calcular_ganhos para retornar o valor do salário fixo.Execução e Comparação: No código principal, crie uma lista contendo um objeto Diarista (ex: 5 dias a R$ 200/dia) e um objeto Mensalista (ex: R$ 4500 fixo). Percorra essa lista usando um loop for e, de forma polimórfica, chame o método calcular_ganhos de cada um, exibindo o nome do profissional e o valor final que ele deve receber.
+class Prestador:
+    def _init_(self, nome):
+        self.nome = nome
+        self.calcular_ganhos = 0.0
+
+class Diarista(Prestador):
+    def _init_(self, nome, quantidade_dias, valor_dia):
+        super()._init_(nome)
+        self.quantidade_dias = quantidade_dias
+        self.valor_dia = valor_dia
+        self.calcular_ganhos = self.quantidade_dias * self.valor_dia
+        print(f"A {self.nome} vai ganhar {self.calcular_ganhos} por {self.quantidade_dias} dias trabalhados ")
+
+class Mensalista(Prestador):
+    def _init_(self, nome, salario_fixo):
+        super()._init_(nome)
+        self.salario_fixo = salario_fixo
+        self.calcular_ganhos = self.salario_fixo
+        print(f"A {self.nome} vai ganhar um salário fixo de {self.calcular_ganhos} pelos dias trabalhados ")
+
+pessoa1 = Diarista("Lia", 5, 200)
+pessoa2 = Mensalista("Sarah", 4500)
--- Aula 08 - Revisão Função e POO/questao9.py
+++ Aula 08 - Revisão Função e POO/questao9.py
@@ -1,1 +1,31 @@
 ##Contexto: Você foi designado para codificar o motor de regras de um carrinho de compras de um e-commerce.Instruções:Composição: Crie uma classe Item que receba nome e preco no construtor. Depois, crie uma classe Carrinho que inicialize uma lista vazia no construtor para armazenar os itens encomendados.Métodos e Protocolos: - Na classe Carrinho, adicione o método adicionar_item que recebe um objeto do tipo Item e o insere na lista.Implemente o método mágico __len__ na classe Carrinho para que ele retorne a quantidade de itens salvos na lista interna.Adicione um método comum chamado calcular_total que varre a lista e soma o preço de cada item.Execução e Comparação: Crie três instâncias de Item com preços diferentes. Instancie o Carrinho. Antes de adicionar qualquer item, exiba a quantidade usando a função nativa len(seu_carrinho). Adicione os itens, exiba novamente o resultado de len(seu_carrinho) para provar o funcionamento do método mágico e, por fim, exiba o valor total da compra gerado pelo calcular_total.
+class Item:
+    def _init_(self, nome, preco):
+        self.nome = nome
+        self.preco = preco
+
+class Carrinho:
+    def _init_(self):
+        self.itens_encomendados = []
+    def adicionar_item(self, item):
+        self.item = item
+        self.itens_encomendados.append(item)##Adiciona os elementos a lista
+    def _len_(self):
+        return len(self.itens_encomendados)#Conta quantos elementos estão n
+    def calcular_total(self):
+        total = 0
+        for item in self.itens_encomendados:
+            total += item.preco
+        return total
+
+item1 = Item("Bola", 100)
+item2 = Item("Barbie", 80)
+item3 = Item("Escova", 150)
+carrinho = Carrinho()
+
+carrinho.adicionar_item(item1)
+carrinho.adicionar_item(item2)
+carrinho.adicionar_item(item3)
+
+print(f"No seu carrinho tem {len(carrinho)} itens")
+print(f"O valor total do seu carrinho é {carrinho.calcular_total()}")
